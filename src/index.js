5c4d27c8c4605bb41e5be57f993aa74f504a238f
kkm <kubasurdej@gmail.com>
2019-09-02 18:37:25 +0200

    Remove program.version from index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const program = require('commander')
 const startMockServer = require('./server')
 const mockSchema = require('./mock')
 
-program.version('0.0.1')
 program
     .option('-f --file <file>', 'schema file location', 'schema.graphql')
     .option('-p --port <port>', 'apollo server port', '4000')
