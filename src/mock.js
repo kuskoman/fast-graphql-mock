693519c86807bfb6b905e12690c328194402adad
kkm <kubasurdej@gmail.com>
2019-09-02 19:43:10 +0200

    Exit process if file does not exist

--- src/mock.js
+++ src/mock.js
@@ -1,7 +1,16 @@
 const graphqlImport = require('graphql-import')
 const graphqlTools = require('graphql-tools')
+const fs = require('fs')
+
+function checkIfFileExist(path) {
+    if (!fs.existsSync(path)) {
+        console.error(`${path} does not exist`)
+        process.exit(1)
+    }
+}
 
 function mockSchema(file) {
+    checkIfFileExist(file)
     const schemaFile = graphqlImport.importSchema(file)
     const schema = graphqlTools.makeExecutableSchema({ typeDefs: schemaFile })
     graphqlTools.addMockFunctionsToSchema({ schema })
