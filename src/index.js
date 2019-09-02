79724d1b512229f72c9835ae99029e251b4cee82
kkm <kubasurdej@gmail.com>
2019-09-02 19:49:47 +0200

    Fix application not reading console arguments

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ program
     .option('-f --file <file>', 'schema file location', 'schema.graphql')
     .option('-p --port <port>', 'apollo server port', '4000')
 
+program.parse(process.argv)
+
 const options = program.opts()
 
 const schema = mockSchema(options.file)
