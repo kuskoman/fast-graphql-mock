3b6df828b8488b0298d6827b06e2cca24c6cd273
kkm <kubasurdej@gmail.com>
2019-09-02 22:03:17 +0200

    Add more fake data types

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,8 +1,13 @@
 const faker = require('faker')
 
 const fakeData = {
-    Float: () => Math.random() * 100,
-    String: () => faker.lorem.words(5)
+    String: () => faker.lorem.words(5),
+    Date: () => faker.date.future(),
+    DateTime: () => faker.date.future(),
+    PostalCode: () => faker.address.zipCode(),
+    PhoneNumber: () => faker.phone.phoneNumber(),
+    URL: () => faker.internet.url(),
+    EmailAdress: () => faker.internet.exampleEmail()
 }
 
 module.exports = fakeData
