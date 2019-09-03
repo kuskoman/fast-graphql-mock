a92c298974612d67184271f12577ce89c33a76a7
kuskoman <kubasurdej@gmail.com>
2019-09-03 21:19:33 +0200

    Separate cli from library (#14)
    * Move cli to separate file
    
    * Add index.js with exports of required functions
    
    * Cleanup exports
    
    * Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-#! /usr/bin/env node
-
-const program = require('commander')
-const startMockServer = require('./server')
+const { createMockServer, startMockServer } = require('./server')
 const mockSchema = require('./mock')
 
-program
-    .option('-f --file <file>', 'schema file location', 'schema.graphql')
-    .option('-p --port <port>', 'apollo server port', '4000')
-
-program.parse(process.argv)
-
-const options = program.opts()
-
-const schema = mockSchema(options.file)
-startMockServer(schema, options.port)
+module.exports = {
+    mockSchema,
+    createMockServer,
+    startMockServer
+}
