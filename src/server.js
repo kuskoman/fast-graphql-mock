2d91793e9f517424f3180fb4024e2a5d1d36216d
kkm <kubasurdej@gmail.com>
2019-09-03 21:06:38 +0200

    Change exported fuctions

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const { ApolloServer } = require('apollo-server')
 const mocks = require('./fakeData')
 
-const createMockServer = (schema, port) => {
-    const server = new ApolloServer({
+const createMockServer = (schema) => {
+    return new ApolloServer({
         schema,
         mocks
     })
+}
 
+const startMockServer = (server, port) => {
     server.listen(port).then(({ url }) => {
         console.log(`Mock server ready at ${url}`);
     })
 }
 
-module.exports = createMockServer
+module.exports = {
+    createMockServer: createMockServer,
+    startMockServer: startMockServer
+}
