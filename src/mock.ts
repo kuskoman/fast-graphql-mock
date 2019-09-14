52c5af8332889d0a3e6fb8a23b7c8728fe736c01
kuskoman <kubasurdej@gmail.com>
2019-09-14 14:43:14 +0200

    Fill missing types (#20)

--- src/mock.ts
+++ src/mock.ts
@@ -1,15 +1,16 @@
 import { importSchema } from 'graphql-import'
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
 import { existsSync } from 'fs'
+import { GraphQLSchema } from 'graphql'
 
-const checkIfFileExist = (path: string) => {
+const checkIfFileExist = (path: string): void => {
     if (!existsSync(path)) {
         console.error(`${path} does not exist`)
         process.exit(1)
     }
 }
 
-const mockSchema = (file: string) => {
+const mockSchema = (file: string): GraphQLSchema => {
     checkIfFileExist(file)
     const schemaFile = importSchema(file)
     const schema = makeExecutableSchema({ typeDefs: schemaFile })
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import { GraphQLSchema } from "graphql"
 import { ApolloServer } from 'apollo-server'
 import fakeData from './fakeData'
 
-export const createMockServer = (schema: GraphQLSchema) => {
+export const createMockServer = (schema: GraphQLSchema): ApolloServer => {
     return new ApolloServer({
         schema,
         mocks: fakeData
     })
 }
 
-export const startMockServer = (server: ApolloServer, port: string | number) => {
+export const startMockServer = (server: ApolloServer, port: string | number): void => {
     server.listen(port).then(({ url }) => {
         console.log(`Mock server ready at ${url}`);
     })
