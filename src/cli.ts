bb291c20eeb234c094c098c04f773f2adc418e6f
kuskoman <kubasurdej@gmail.com>
2019-09-14 15:35:24 +0200

    General fixes (#21)
    * Create npmignore
    
    * Move cli file content to main function
    
    * Use new export syntax in index file

--- src/cli.ts
+++ src/cli.ts
@@ -4,14 +4,18 @@ import program from 'commander'
 import { createMockServer, startMockServer } from './server'
 import mockSchema from './mock'
 
-program
-    .option('-f --file <file>', 'schema file location', 'schema.graphql')
-    .option('-p --port <port>', 'apollo server port', '4000')
+const main = (): void => {
+    program
+        .option('-f --file <file>', 'schema file location', 'schema.graphql')
+        .option('-p --port <port>', 'apollo server port', '4000')
 
-program.parse(process.argv)
+    program.parse(process.argv)
 
-const options = program.opts()
+    const options = program.opts()
+    const schema = mockSchema(options.file)
+    const server = createMockServer(schema)
 
-const schema = mockSchema(options.file)
-const server = createMockServer(schema)
-startMockServer(server, options.port)
+    startMockServer(server, options.port)
+}
+
+main()
