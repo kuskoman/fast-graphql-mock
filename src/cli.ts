4df560a3b30f367989b5b0213c91752761cba4ee
kkm <kubasurdej@gmail.com>
2019-09-14 13:34:36 +0200

    Change way of importing commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-import * as program from 'commander'
+import program from 'commander'
 import { createMockServer, startMockServer } from './server'
 import mockSchema from './mock'
 
