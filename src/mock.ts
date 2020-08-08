ebcc3ec3fc7ce5342375d712ccb64bcc6be00920
kkm <kubasurdej@gmail.com>
2020-08-08 22:47:28 +0200

    Modify mock file to use current version of graphql-tools

--- src/mock.ts
+++ src/mock.ts
@@ -1,22 +1,23 @@
-import { importSchema } from 'graphql-import'
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
-import { existsSync } from 'fs'
-import { GraphQLSchema } from 'graphql'
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { existsSync } from "fs";
+import { GraphQLSchema } from "graphql";
 
 const checkIfFileExist = (path: string): void => {
-    if (!existsSync(path)) {
-        console.error(`${path} does not exist`)
-        process.exit(1)
-    }
-}
+  if (!existsSync(path)) {
+    console.error(`${path} does not exist`);
+    process.exit(1);
+  }
+};
 
 const mockSchema = (file: string): GraphQLSchema => {
-    checkIfFileExist(file)
-    const schemaFile = importSchema(file)
-    const schema = makeExecutableSchema({ typeDefs: schemaFile })
-    addMockFunctionsToSchema({ schema })
+  checkIfFileExist(file);
+  const schema = loadSchemaSync(file, {
+    loaders: [new GraphQLFileLoader()],
+  });
 
-    return schema
-}
+  return addMocksToSchema({ schema });
+};
 
-export default mockSchema
+export default mockSchema;
